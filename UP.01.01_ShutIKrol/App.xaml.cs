d84885bc9c3038b0f70028a18294c742757547de
belochka-228 <sofikrohmal@gmail.com>
2026-04-26 18:31:09 +0300

    Добавьте файлы проекта.

---
+++ UP.01.01_ShutIKrol/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace UP._01._01_ShutIKrol
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
